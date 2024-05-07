a4d1c63ce10b07463ff43d2086ebd3646c372566
saengx <157899625+saengx@users.noreply.github.com>
2024-05-07 09:19:23 +0700

    Update config.py

--- config.py
+++ config.py
@@ -4,11 +4,10 @@ import os, json, requests
 def banner():
     os.system("clear")
     print("\033[1;34;40m")
-    os.system("figlet -f ANSI_Shadow VERUS")
-    os.system("figlet -f digital server version")
+    os.system("figlet -f ANSI_Shadow ghost")
+    os.system("figlet -f ANSI_Shadow rider")
     print("\033[00m\n")
     print("\033[96mEdit by PICHET SAENGTEWAN\033[0m")
-    print("\033[96mDebug Colors by AMARIN PRAPRIWIT\033[0m")
     print("\033[36m\033[0m")
     
     print(f"\n\033[91mกำลัง Run Mining\033[0m\n")
